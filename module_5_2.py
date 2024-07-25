8bef331cfc261967e90315e5c06677dcc94546b0
Edison <krylov2310@gmail.com>
2024-07-25 19:07:45 +0500

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,35 @@
+task = 'Домашняя работа по уроку "Специальные методы классов"'
+avtor = 'Студент Крылов Эдуард Васильевич'
+thanks = 'Благодарю за внимание :-)'
+print()
+print(task)
+print(avtor)
+print()
+
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        talk = f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+        return talk
+
+
+# Пример выполняемого кода:
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+# __str__
+print(h1)
+print(h2)
+# __len__
+print(len(h1))
+print(len(h2))
+print()
+print(thanks)
